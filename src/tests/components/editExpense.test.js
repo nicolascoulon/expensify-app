e5dc3ba9ac3f1a6acbe0ac06479dbe6a16109063
Nicolas Coulon <nicocoulon@gmail.com>
2017-12-08 15:31:53 +0100

    remove db and test v2

--- src/tests/components/editExpense.test.js
+++ src/tests/components/editExpense.test.js
@@ -3,16 +3,16 @@ import { shallow } from 'enzyme';
 import { EditExpensePage } from '../../components/EditExpensePage';
 import expenses from '../fixtures/expenses';
 
-let editExpense, removeExpense, history, wrapper;
+let editExpense, startRemoveExpense, history, wrapper;
 
 beforeEach(() => {
     editExpense = jest.fn();
-    removeExpense = jest.fn();
+    startRemoveExpense = jest.fn();
     history = { push: jest.fn() };
     wrapper = shallow(
         <EditExpensePage
             editExpense={editExpense}
-            removeExpense={removeExpense}
+            startRemoveExpense={startRemoveExpense}
             history={history}
             expense={expenses[2]}
         />
@@ -32,7 +32,7 @@ test('should handle edit expense', () => {
 test('should handle remove expense', () => {
   wrapper.find('button').prop('onClick')(expenses[2]);
     expect(history.push).toHaveBeenLastCalledWith('/');
-    expect(removeExpense).toHaveBeenLastCalledWith({
+    expect(startRemoveExpense).toHaveBeenLastCalledWith({
         id: expenses[2].id
     });
 });
