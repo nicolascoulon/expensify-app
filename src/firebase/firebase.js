121434b40e54de792a4ed354c74d4cb7cf2559d0
Nicolas Coulon <nicocoulon@gmail.com>
2017-12-08 10:37:22 +0100

    setup tes db env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyAbngvgAWnwQJ5hEzsKLsOsWGNb0CY__j4",
-    authDomain: "expensify-d64f5.firebaseapp.com",
-    databaseURL: "https://expensify-d64f5.firebaseio.com",
-    projectId: "expensify-d64f5",
-    storageBucket: "expensify-d64f5.appspot.com",
-    messagingSenderId: "727781192140"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,10 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv';
 
+// enzyme for react v16 => mega relou
 Enzyme.configure({
     adapter: new Adapter()
 });
+// switch env 
+DotEnv.config({ path: '.env.test'});
